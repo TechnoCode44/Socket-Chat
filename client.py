32d378dc257fe732bfc2edd1e0792483670738a3
TechnoCode44 <ElmoSmiley@outlook.com>
2023-03-06 20:59:05 +0000

    Added recive function

--- client.py
+++ client.py
@@ -6,3 +6,6 @@ server.connect(("192.168.1.79", 4000))
 
 def send(data): # Sends encrypted data to server
     server.send(dumps(data))
+
+def recive(data_length: int = 1024): # Decodes encrypted data recive from server
+    return loads(server.recv(data_length))
