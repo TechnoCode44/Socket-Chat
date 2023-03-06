515f538192b4558892a2cdcaae15adc1877afffc
TechnoCode44 <ElmoSmiley@outlook.com>
2023-03-06 20:57:20 +0000

    Added send function

--- client.py
+++ client.py
@@ -4,4 +4,5 @@ from pickle import dumps, loads
 server = socket(AF_INET, SOCK_STREAM)
 server.connect(("192.168.1.79", 4000))
 
-print(loads(server.recv(1024)))
+def send(data): # Sends encrypted data to server
+    server.send(dumps(data))
