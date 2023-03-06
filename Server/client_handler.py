f700decc6d1c0e75b1df9892b122110bd656c60d
TechnoCode44 <ElmoSmiley@outlook.com>
2023-03-06 21:29:12 +0000

    Added mulithreading

--- Server/client_handler.py
+++ Server/client_handler.py
@@ -2,4 +2,5 @@ from socket import socket
 from pickle import dumps, loads
 
 def client_handler(client: socket, clients_data):
-    pass
+    client.send(dumps("Hello, World!"))
+    client.close()
--- Server/server.py
+++ Server/server.py
@@ -1,10 +1,16 @@
 from socket import socket, AF_INET, SOCK_STREAM
-from pickle import dumps, loads
+from threading import Thread
+from client_handler import *
 
 server = socket(AF_INET, SOCK_STREAM)
 server.bind(("0.0.0.0", 4000))
 server.listen()
 
+threads = []
+clients_data = []
+
 while True:
     client, address = server.accept()
-    client.send(dumps("Hello, World"))
+    clients_data.append({})
+    threads.append(Thread(target=client_handler, args=(client, clients_data[-1])))
+    threads[-1].start()
