7b170bc71c42d38778af592dd0291354f7006610
TechnoCode44 <ElmoSmiley@outlook.com>
2023-03-06 20:59:53 +0000

    Client sends username when connected

--- client.py
+++ client.py
@@ -9,3 +9,9 @@ def send(data): # Sends encrypted data to server
 
 def recive(data_length: int = 1024): # Decodes encrypted data recive from server
     return loads(server.recv(data_length))
+
+if __name__ == "__main__":
+    username = input("What would you like to be called? ")
+    send(username)
+    while True:
+        pass
