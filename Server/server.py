fb5dc7f419f211a0859fafe8927b925f4e2c91cc
TechnoCode44 <ElmoSmiley@outlook.com>
2023-03-07 21:17:33 +0000

    Added clients list

--- Server/server.py
+++ Server/server.py
@@ -6,9 +6,11 @@ server = socket(AF_INET, SOCK_STREAM)
 server.bind(("0.0.0.0", 4000))
 server.listen()
 
+clients = [] # List with clients object's
 clients_data = []
 
 while True:
     client, address = server.accept()
     clients_data.append({})
+    clients.append(client)
     Thread(target=client_handler, args=(client, clients_data[-1])).start()
