6b5f1468e879bf529f0932346cfa06472fcdba76
TechnoCode44 <ElmoSmiley@outlook.com>
2023-03-07 21:10:50 +0000

    Added recive Thread

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from pickle import dumps, loads
+from threading import Thread
 
 server = socket(AF_INET, SOCK_STREAM)
 server.connect(("192.168.1.79", 4000))
@@ -7,11 +8,18 @@ server.connect(("192.168.1.79", 4000))
 def send(data): # Sends encrypted data to server
     server.send(dumps(data))
 
-def recive(data_length: int = 1024): # Decodes encrypted data recive from server
-    return loads(server.recv(data_length))
+def recive(data_length: int = 1024): # Recives data from server and prints it
+    data = server.recv(data_length)
+    data = loads(data)
+    name = data["Name"]
+    message = data["Message"]
+    print(f"[{name}] {message}")
 
 if __name__ == "__main__":
     username = input("What would you like to be called? ")
     send(username)
+    
+    Thread(target=recive).start()
+
     while True:
         pass
