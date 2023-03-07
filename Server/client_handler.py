1cd7adb70c31047e1d9718fd258f7507cc5d69bf
TechnoCode44 <ElmoSmiley@outlook.com>
2023-03-07 20:45:32 +0000

    Added send and recive functions for client handler

--- Server/client_handler.py
+++ Server/client_handler.py
@@ -2,5 +2,10 @@ from socket import socket
 from pickle import dumps, loads
 
 def client_handler(client: socket, clients_data):
-    client.send(dumps("Hello, World!"))
+    def send(data): # Sends encrypted data to client
+        client.send(dumps(data))
+
+    def recive(data_length: int = 1024): # Decodes encrypted data recive from client
+        return loads(client.recv(data_length))
+    
     client.close()
