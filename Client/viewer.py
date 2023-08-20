bb7cd9fdd9019365f8a3c78ddd8d2aacfcfdd8e5
TechnoCode44 <ElmoSmiley@outlook.com>
2023-08-20 13:31:57 +0100

    Copied viewing code to viewer.py

---
+++ Client/viewer.py
@@ -0,0 +1,17 @@
+from socket import socket, AF_INET, SOCK_STREAM
+from pickle import loads
+from os import _exit as exit
+
+server = socket(AF_INET, SOCK_STREAM)
+server.connect(("localhost", 4001))
+
+if __name__ == "__main__":
+    while True:
+        data = server.recv(1024)
+        if not data: # Disconnected
+            exit(1)
+        else:
+            data = loads(data)
+            name = data["Name"]
+            message = data["Message"]
+            print(f"[{name}] {message}")
