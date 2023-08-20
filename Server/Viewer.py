8dd72c7c40ccfaf83670e81f1cb901841793a363
TechnoCode44 <ElmoSmiley@outlook.com>
2023-08-20 14:02:25 +0100

    Added Viewer class

---
+++ Server/Viewer.py
@@ -0,0 +1,16 @@
+from pickle import dumps
+
+class Viewer:
+    def __init__(self, client, address) -> None:
+        self.client = client
+        self.address = address
+
+        print(f"[{address}] Viewer has connected")
+
+    def send(self, data):
+        try:
+            encrpyted_data = dumps(data)
+            self.client.send(encrpyted_data)
+        except EOFError:
+            print(f"[{self.address}] Viewer disconnected")
+            self.client.close()
