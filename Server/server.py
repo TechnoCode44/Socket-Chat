51405af17805a639c8c5c4f05827bd146e6522f8
TechnoCode44 <ElmoSmiley@outlook.com>
2023-08-20 13:41:53 +0100

    Created client reciver function

--- Server/server.py
+++ Server/server.py
@@ -2,15 +2,19 @@ from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
 from client_handler import *
 
-server = socket(AF_INET, SOCK_STREAM)
-server.bind(("0.0.0.0", 4000))
-server.listen()
-
 clients = [] # List with clients object's
 clients_data = []
 
-while True:
-    client, address = server.accept()
-    clients_data.append({})
-    clients.append(client)
-    Thread(target=client_handler, args=(client, clients_data[-1], clients, address[0])).start()
+def client_reciver():
+    server = socket(AF_INET, SOCK_STREAM)
+    server.bind(("0.0.0.0", 4000))
+    server.listen()
+
+    while True:
+        client, address = server.accept()
+        clients_data.append({})
+        clients.append(client)
+        Thread(target=client_handler, args=(client, clients_data[-1], clients, address[0])).start()
+
+if __name__ == "__main__":
+    client_reciver()
